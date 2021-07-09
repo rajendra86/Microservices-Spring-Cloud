8066b98835d55eb1c562388685acb3d33b8e11bb
Rajendra Prasad K <rajendra86@msn.com>
2021-07-09 16:54:13 +0530

    Update data.sql

--- spring-security-oauth-example/src/main/resources/data.sql
+++ spring-security-oauth-example/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT into oauth_client_details(client_id, resource_ids, client_secret, scope, authorized_grant_types,
 authorities, access_token_validity, refresh_token_validity) values('DEV', 'rest_api', 'DEV',
-'trust,read,write', 'client_credentials, authorization_code, implicit, password, refresh_token',
+'trust,read,write', 'client_credentials,authorization_code,implicit,password,refresh_token',
 'ROLE_CLIENT, ROLE_TRUSTED_CLIENT', '600','3600');
 
 INSERT INTO oauth_user_details(service_id, service_name, service_description, username, password, service_token, 
